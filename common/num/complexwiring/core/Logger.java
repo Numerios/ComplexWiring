91f07c62f75980373d9033e85b93bdff6639ecbf
Numerios <mrnumerios@gmail.com>
2013-06-29 11:47:21 +0200

    Added a logger

---
+++ common/num/complexwiring/core/Logger.java
@@ -0,0 +1,29 @@
+package num.complexwiring.core;
+
+import java.util.logging.Level;
+
+import num.complexwiring.lib.Reference;
+
+public class Logger {
+    private static java.util.logging.Logger cwLogger = java.util.logging.Logger.getLogger(Reference.MOD_ID);
+
+    public static void log(Level level, String message) {
+        cwLogger.log(level, message);
+    }
+
+    public static void info(String message) {
+        log(Level.INFO, message);
+    }
+
+    public static void warn(String message) {
+        log(Level.WARNING, message);
+    }
+
+    public static void fine(String message) {
+        log(Level.FINE, message);
+    }
+
+    public static void debug(String message) {
+        log(Level.WARNING, "[DEBUG] " + message);
+    }
+}
