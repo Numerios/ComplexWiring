2fddc4ae365983df732f69bc4c7a68fa1c41d94c
Numerios <mrnumerios@gmail.com>
2013-07-06 13:53:00 +0200

    Added a new util class with a coord system (MCVector3) world, x, y, z

---
+++ common/num/complexwiring/util/MathUtil.java
@@ -0,0 +1,102 @@
+package num.complexwiring.util;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.ForgeDirection;
+
+public class MathUtil {
+    public static class MCVector3 {
+        public int x, y, z;
+        public IBlockAccess world;
+
+        public MCVector3(IBlockAccess world, int x, int y, int z) {
+            this.world = world;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public MCVector3(TileEntity tile) {
+            this.world = tile.worldObj;
+            this.x = tile.xCoord;
+            this.y = tile.yCoord;
+            this.z = tile.zCoord;
+        }
+
+        public MCVector3 copy() {
+            return new MCVector3(this.world, this.x, this.y, this.z);
+        }
+
+        public TileEntity toTile() {
+            return this.world.getBlockTileEntity(this.x, this.y, this.z);
+        }
+
+        public MCVector3 getNeighbour(ForgeDirection side) {
+            switch (side) {
+                case EAST: {
+                    x++;
+                    break;
+                }
+                case WEST: {
+                    x--;
+                    break;
+                }
+                case UP: {
+                    y++;
+                    break;
+                }
+                case DOWN: {
+                    y--;
+                    break;
+                }
+                case SOUTH: {
+                    z++;
+                    break;
+                }
+                case NORTH: {
+                    z--;
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            return this;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof MCVector3))
+                return false;
+
+            MCVector3 vec3 = (MCVector3) obj;
+
+            if (this.x != vec3.x)
+                return false;
+            if (this.y != vec3.y)
+                return false;
+            if (this.z != vec3.z)
+                return false;
+            if (this.world.equals(vec3.world) == false)
+                return false;
+
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "[x: " + this.x + ", y:" + this.y + ", z:" + this.z + "]";
+        }
+
+        @Override
+        public int hashCode() {
+            int nl = (32 - 1); // 2 ^ 5 - 1
+            int hashCode = this.x;
+
+            hashCode *= nl + this.y;
+            hashCode *= nl + this.z;
+            hashCode *= nl + this.world.hashCode();
+            return hashCode;
+        }
+    }
+}
