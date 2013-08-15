fa813f588e6a04f1ecd083ff017c269cec66d311
Numerios <mrnumerios@gmail.com>
2013-08-16 01:36:25 +0200

    Connectable is now a name for any itemConnectable (will rename soon[tm])

--- common/num/complexwiring/api/IConnectable.java
+++ common/num/complexwiring/api/IConnectable.java
@@ -1,6 +1,7 @@
 package num.complexwiring.api;
 
 import net.minecraftforge.common.ForgeDirection;
+import num.complexwiring.core.Wrapper;
 
 /**
  * IConnectable is an interface for all connectable tiles in ComplexWiring
@@ -12,11 +13,22 @@ public interface IConnectable {
      * 
      * @param side
      *            The side that is checked
-     * @param connectable
-     *            The tile checked
      * @return True if the tile is connected to another on given side, false if
      *         not.
      */
-    public boolean getConnection(ForgeDirection side, IConnectable connectable);
+    public boolean getConnection(ForgeDirection side);
+
+    /**
+     * Checks if tile can accept Wrappers (Items carrying ItemStacks)
+     * @param wrapper The wrapper we want to check
+     * @return True if tile accepts wrappers, false if not.
+     */
+    public boolean canAccept(Wrapper wrapper);
+    
+    /**
+     * Orders the tile to accept a wrapper
+     * @param wrapper The accepted wrapper
+     */
+    public void acceptWrapper(Wrapper wrapper);
 
 }
