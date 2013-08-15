32437d2de42b48253199e14e54a179683cde3b5a
Numerios <mrnumerios@gmail.com>
2013-08-15 22:47:49 +0200

    WireHelper has a wonderful method returning ArrayList of connected sides

--- common/num/complexwiring/util/WireHelper.java
+++ common/num/complexwiring/util/WireHelper.java
@@ -1,20 +1,31 @@
 package num.complexwiring.util;
 
+import java.util.ArrayList;
+
 import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
 import num.complexwiring.api.IConnectable;
-import num.complexwiring.api.IItemWire;
 
 public class WireHelper {
     public static boolean getConnection(ForgeDirection side, IConnectable connectable) {
         TileEntity tile = MCVector3.get((TileEntity) connectable).getNeighbour(side).toTile();
-        if (tile instanceof IItemWire) {
+        if (tile instanceof IConnectable) { // CW connections
             return true;
-        } else if (tile instanceof IInventory) {
+        } else if (tile instanceof IInventory) { // default MC connections
             return true;
         } else {
             return false;
         }
+    }
+
+    public static ArrayList<ForgeDirection> getAllConnections(IConnectable connectable) {
+        ArrayList<ForgeDirection> connected = new ArrayList<ForgeDirection>();
+        for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS) {
+            if (WireHelper.getConnection(side, connectable)) {
+                connected.add(side);
+            }
+        }
+        return connected;
     }
 }
