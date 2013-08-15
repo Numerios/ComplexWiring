c461be323fe9efb6453a660d2186c2169a77fd7f
Numerios <mrnumerios@gmail.com>
2013-08-16 01:41:04 +0200

    Rendering wrappers in the main renderer now!

--- common/num/complexwiring/render/WireModelRenderer.java
+++ common/num/complexwiring/render/WireModelRenderer.java
@@ -5,7 +5,10 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
 import num.complexwiring.api.IConnectable;
-import num.complexwiring.api.IItemWire;
+import num.complexwiring.block.TileItemWire;
+import num.complexwiring.core.Logger;
+import num.complexwiring.core.Wrapper;
+
 import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
@@ -16,88 +19,86 @@ public class WireModelRenderer extends TileEntitySpecialRenderer {
 
     public static final ModelWire model = new ModelWire();
 
-    public void renderModelTwo(IConnectable wire, double x, double y, double z) {
+    public void renderModel(IConnectable wire, double x, double y, double z) {
         Minecraft.getMinecraft().renderEngine.bindTexture("/mods/ComplexWiring/textures/blocks/wireModel.png");
 
         GL11.glPushMatrix();
         GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
         GL11.glScalef(1.0F, -1F, -1F);
-        // System.out.println("north: " + north + "; south: " + south +
-        // "; west: " + west + "; east: " + east + "; up: "
-        // + up + "; down: " + down);
-        if (wire.getConnection(ForgeDirection.SOUTH, wire)) {
+
+        if (wire.getConnection(ForgeDirection.SOUTH)) {
             model.front1.render(0.0625F);
             model.front2.render(0.0625F);
             model.front3.render(0.0625F);
             model.front4.render(0.0625F);
         } else {
-            if (!wire.getConnection(ForgeDirection.EAST, wire)) {
+            if (!wire.getConnection(ForgeDirection.EAST)) {
                 model.centerFront2.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.WEST, wire)) {
+            if (!wire.getConnection(ForgeDirection.WEST)) {
                 model.centerFront1.render(0.0625F);
             }
         }
-        if (wire.getConnection(ForgeDirection.WEST, wire)) {
+        if (wire.getConnection(ForgeDirection.WEST)) {
             model.right1.render(0.0625F);
             model.right2.render(0.0625F);
             model.right3.render(0.0625F);
             model.right4.render(0.0625F);
         }
-        if (wire.getConnection(ForgeDirection.NORTH, wire)) {
+        if (wire.getConnection(ForgeDirection.NORTH)) {
             model.back1.render(0.0625F);
             model.back2.render(0.0625F);
             model.back3.render(0.0625F);
             model.back4.render(0.0625F);
         } else {
-            if (!wire.getConnection(ForgeDirection.EAST, wire)) {
+            if (!wire.getConnection(ForgeDirection.EAST)) {
                 model.centerBack1.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.WEST, wire)) {
+            if (!wire.getConnection(ForgeDirection.WEST)) {
                 model.centerBack2.render(0.0625F);
             }
         }
-        if (wire.getConnection(ForgeDirection.EAST, wire)) {
+        if (wire.getConnection(ForgeDirection.EAST)) {
             model.left1.render(0.0625F);
             model.left2.render(0.0625F);
             model.left3.render(0.0625F);
             model.left4.render(0.0625F);
         }
-        if (wire.getConnection(ForgeDirection.UP, wire)) {
+        if (wire.getConnection(ForgeDirection.UP)) {
             model.up1.render(0.0625F);
             model.up2.render(0.0625F);
             model.up3.render(0.0625F);
             model.up4.render(0.0625F);
         } else {
-            if (!wire.getConnection(ForgeDirection.SOUTH, wire)) {
+            if (!wire.getConnection(ForgeDirection.SOUTH)) {
                 model.centerUp2.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.NORTH, wire)) {
+            if (!wire.getConnection(ForgeDirection.NORTH)) {
                 model.centerUp1.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.EAST, wire)) {
+            if (!wire.getConnection(ForgeDirection.EAST)) {
                 model.centerLeft1.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.WEST, wire)) {
+            if (!wire.getConnection(ForgeDirection.WEST)) {
                 model.centerRight1.render(0.0625F);
             }
         }
-        if (wire.getConnection(ForgeDirection.DOWN, wire)) {
+        if (wire.getConnection(ForgeDirection.DOWN)) {
             model.down1.render(0.0625F);
             model.down2.render(0.0625F);
             model.down3.render(0.0625F);
             model.down4.render(0.0625F);
         } else {
-            if (!wire.getConnection(ForgeDirection.SOUTH, wire)) {
+            if (!wire.getConnection(ForgeDirection.SOUTH)) {
                 model.centerDown2.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.NORTH, wire)) {
+            if (!wire.getConnection(ForgeDirection.NORTH)) {
                 model.centerDown1.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.EAST, wire)) {
+            if (!wire.getConnection(ForgeDirection.EAST)) {
                 model.centerLeft2.render(0.0625F);
             }
-            if (!wire.getConnection(ForgeDirection.WEST, wire)) {
+            if (!wire.getConnection(ForgeDirection.WEST)) {
                 model.centerRight2.render(0.0625F);
             }
         }
@@ -107,7 +108,10 @@ public class WireModelRenderer extends TileEntitySpecialRenderer {
 
     @Override
     public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float f) {
-        IItemWire wire = (IItemWire) tile;
-        renderModelTwo(wire, x, y, z);
+        TileItemWire wire = (TileItemWire) tile;
+        renderModel(wire, x, y, z);
+        for (Wrapper wrapper : wire.contents) {
+            WrapperRenderer.render(wire, x, y, z, wrapper);
+        }
     }
 }
