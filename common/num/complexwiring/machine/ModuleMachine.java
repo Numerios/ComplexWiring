db281556cdc9998629ef177c3e37151a4069a796
Numerios <MrNumerios@gmail.com>
2013-12-01 11:17:59 +0100

    Very rudimentary recipe system

--- common/num/complexwiring/machine/ModuleMachine.java
+++ common/num/complexwiring/machine/ModuleMachine.java
@@ -3,6 +3,8 @@ package num.complexwiring.machine;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import num.complexwiring.lib.ModuleBase;
+import num.complexwiring.world.BlockOrePrimary;
+import num.complexwiring.world.BlockOreSecondary;
 
 public class ModuleMachine extends ModuleBase {
     public static Block machineBasic;
@@ -12,5 +14,11 @@ public class ModuleMachine extends ModuleBase {
         machineBasic = new BlockMachineBasic(668);
         GameRegistry.registerBlock(machineBasic, machineBasic.getUnlocalizedName());
         GameRegistry.registerTileEntity(TileEntityMachineBasic.class, machineBasic.getUnlocalizedName());
+    }
+
+    public void init() {
+        MachineBasicRecipes.add(BlockOrePrimary.EnumOrePrimary.COPPER.getIS(1), BlockOreSecondary.EnumOreSecondary.CHALCOPYRITE.getIS(1));
+        MachineBasicRecipes.add(BlockOrePrimary.EnumOrePrimary.TIN.getIS(1), BlockOreSecondary.EnumOreSecondary.CASSITERITE.getIS(1));
+
     }
 }
