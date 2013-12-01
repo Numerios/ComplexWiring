f0dfab206a0609da285c3bdc635a8d629cbfdac9
JBuk <jbuk@jbuk.eu>
2013-12-01 22:01:20 +0100

    Everything to static!

--- common/num/complexwiring/recipe/RecipeManager.java
+++ common/num/complexwiring/recipe/RecipeManager.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public class RecipeManager {
 
-    private List<Recipe> recipes = new ArrayList<Recipe>();
+    private static List<Recipe> recipes = new ArrayList<Recipe>();
 
-    public void add(Recipe recipe) {
+    public static void add(Recipe recipe) {
         recipes.add(recipe);
     }
 
-    public Recipe get(ItemStack is) {
+    public static Recipe get(ItemStack is) {
         if (is != null) {
             for (Recipe recipe : recipes) {
                 if (recipe.matches(is)) {
@@ -25,7 +25,7 @@ public class RecipeManager {
         return null;
     }
 
-    public void addAllRecipes() {
+    public static void addAllRecipes() {
         add(new Recipe(new ItemStack(Item.appleGold, 1), new RecipeOutput(new ItemStack(Item.appleRed, 1), 0.8F), new RecipeOutput(new ItemStack(Item.goldNugget, 3), 0.2F)));
     }
 }
