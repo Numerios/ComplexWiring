611d561c2e992a9cac6b0e10bbfc7f68ba5797df
Numerios <MrNumerios@gmail.com>
2013-12-06 22:38:42 +0100

    You ain't seen nuthin' !

--- common/num/complexwiring/lib/Strings.java
+++ common/num/complexwiring/lib/Strings.java
@@ -17,21 +17,16 @@ public class Strings {
     public static final String ORE_BITUMINOUS_NAME = "oreBituminous";
     public static final String ORE_ANTHRACITE_NAME = "oreAnthracite";
 
-<<<<<<< HEAD
     public static final String INGOT_COPPER_NAME = "ingotCopper";
     public static final String INGOT_TIN_NAME = "ingotTin";
     public static final String INGOT_SILVER_NAME = "ingotSilver";
     public static final String INGOT_LEAD_NAME = "ingotLead";
 
-    public static final String MACHINE_ORELYZER_NAME = "machineOrelyzer";
-    public static final String MACHINE_ORELYZER_DESC = StatCollector.translateToLocal("tile.complexwiring.machine.orelyzer.desc");   //TODO MAKE THIS PRETTIER
-=======
     public static final String MACHINE_BASIC_ORELYZER_NAME = "machineBasicOrelyzer";
     public static final String MACHINE_BASIC_ORELYZER_DESC = StatCollector.translateToLocal("tile.complexwiring.machine.basic.orelyzer.desc");   //TODO MAKE THIS PRETTIER
 
     public static final String MACHINE_POWERED_ORELYZER_NAME = "machinePoweredOrelyzer";
     public static final String MACHINE_POWERED_ORELYZER_DESC = StatCollector.translateToLocal("tile.complexwiring.machine.powered.orelyzer.desc");   //TODO MAKE THIS PRETTIER
->>>>>>> powered
 
     public static final String TABLET_NAME = "tablet";
 }
