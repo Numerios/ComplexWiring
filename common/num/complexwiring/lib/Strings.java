51001b5c00d2a7a5af473a1dd31fe006c5db4b58
JBuk <jbuk@jbuk.eu>
2013-12-08 11:19:13 +0100

    Lots of secondary ores

--- common/num/complexwiring/lib/Strings.java
+++ common/num/complexwiring/lib/Strings.java
@@ -12,11 +12,72 @@ public class Strings {
     public static final String ORE_TUNGSTEN_NAME = "oreTungsten";
     public static final String ORE_TITANIUM_NAME = "oreTitanium";
 
+    // COPPER Secondary
+    public static final String ORE_CHALCOCITE_NAME = "oreChalcocite";
+    public static final String ORE_BORNITE_NAME = "oreBornite";
     public static final String ORE_CHALCOPYRITE_NAME = "oreChalcopyrite";
-    public static final String ORE_CASSITERITE_NAME = "oreCassiterite";
-    public static final String ORE_ARGENTITE_NAME = "oreArgentite";
-    public static final String ORE_GALENA_NAME = "oreGalena";
+    public static final String ORE_MALACHITE_NAME = "oreMalachite";
 
+    // TIN Secondary
+    public static final String ORE_CASSITERITE_NAME = "oreCassiterite";
+    public static final String ORE_STANNITE_NAME = "oreStannite";
+
+    // SILVER Secondary
+    public static final String ORE_ARGENTINE_NAME = "oreArgentine";
+    public static final String ORE_CHLORARGYTE_NAME = "oreChlorargyte";
+
+    // LEAD Secondary
+    public static final String ORE_GALENITE_NAME = "oreGalenite";
+    public static final String ORE_CERUSSITE_NAME = "oreCerussite";
+    public static final String ORE_ANGLESITE_NAME = "oreAnglesite";
+
+    // ALUMINIUM Secondary
+    public static final String ORE_BAUXITE_NAME = "oreBauxite";
+    public static final String ORE_CRYOLITE_NAME = "oreCryolite";
+
+    // URANIUM Secondary
+    public static final String ORE_URANINITE_NAME = "oreUraninite";
+    public static final String ORE_AUTUNITE_NAME = "oreAutunite";
+
+    // TUNGSTEN Secondary
+    public static final String ORE_WOLFRAMITE_NAME = "oreWolframite";
+    public static final String ORE_SCHEELITE_NAME = "oreScheelite";
+
+    // TITANIUM Secondary
+    public static final String ORE_ILMENITE_NAME = "oreIlmenite";
+    public static final String ORE_RUTILE_NAME = "oreRutile";
+
+    // GOLD Secondary
+    public static final String ORE_CALAVERITE_NAME = "oreCalaverite";
+    public static final String ORE_SYLVANITE_NAME = "oreSylvanite";
+    public static final String ORE_PUREGOLD_NAME = "orePureGold";
+
+    // IRON Secondary
+    public static final String ORE_MAGNETITE_NAME = "oreMagnetite";
+    public static final String ORE_HEMATITE_NAME = "oreHematite";
+    public static final String ORE_SIDERITE_NAME = "oreSiderite";
+    public static final String ORE_CHROMITE_NAME = "oreChromite";
+    public static final String ORE_PENTLADITE_NAME = "orePentlandite";
+
+    // STONE/COBBLE Secondary
+    public static final String ORE_PYROLUSITE_NAME = "orePyrolusite";
+    public static final String ORE_CINNABAR_NAME = "oreCinnabar";
+    public static final String ORE_DOLOMITE_NAME = "oreDolomite";
+    public static final String ORE_SPHALERITE_NAME = "oreSphalerite";
+    public static final String ORE_LIMESTONE_NAME = "oreLimestone";
+    public static final String ORE_LIME_NAME = "oreLime";
+    public static final String ORE_COBALTITE_NAME = "oreCobaltite";
+
+    // CLAY Secondary
+    public static final String ORE_LITHIUM_NAME = "oreLithium";
+
+    // LAPIS Secondary
+    public static final String ORE_LAZURITE_NAME = "oreLazurite";
+
+    // EMERALD Secondary
+    public static final String ORE_BERYL_NAME = "oreBeryl";
+
+    // COAL Secondary
     public static final String ORE_LIGNITE_NAME = "oreLignite";
     public static final String ORE_SUBBITUMINOUS_NAME = "oreSubBituminous";
     public static final String ORE_BITUMINOUS_NAME = "oreBituminous";
--- common/num/complexwiring/world/ore/EnumOreSecondary.java
+++ common/num/complexwiring/world/ore/EnumOreSecondary.java
@@ -9,10 +9,58 @@ import num.complexwiring.lib.Strings;
 import num.complexwiring.world.ModuleWorld;
 
 public enum EnumOreSecondary {
+
+    CHALCOCITE(Strings.ORE_CHALCOCITE_NAME, "oreCopper"),
+    BORNITE(Strings.ORE_BORNITE_NAME, "oreCopper"),
     CHALCOPYRITE(Strings.ORE_CHALCOPYRITE_NAME, "oreCopper"),
+    MALACHITE(Strings.ORE_MALACHITE_NAME, "oreCopper"),
+
     CASSITERITE(Strings.ORE_CASSITERITE_NAME, "oreTin"),
-    ARGENTITE(Strings.ORE_ARGENTITE_NAME, "oreSilver"),
-    GALENA(Strings.ORE_GALENA_NAME, "oreLead"),
+    STANNITE(Strings.ORE_STANNITE_NAME, "oreTin"),
+
+    ARGENTINE(Strings.ORE_ARGENTINE_NAME, "oreSilver"),
+    CHLORARGYTE(Strings.ORE_CHLORARGYTE_NAME, "oreSilver"),
+
+    GALENITE(Strings.ORE_GALENITE_NAME, "oreLead"),
+    CERUSSITE(Strings.ORE_CERUSSITE_NAME, "oreLead"),
+    ANGLESITE(Strings.ORE_ANGLESITE_NAME, "oreLead"),
+
+    BAUXITE(Strings.ORE_BAUXITE_NAME, "oreAluminium"),
+    CRYOLITE(Strings.ORE_CRYOLITE_NAME, "oreAluminium"),
+
+    URANITE(Strings.ORE_URANINITE_NAME, "oreUranium"),
+    AUTUNITE(Strings.ORE_AUTUNITE_NAME, "oreUranium"),
+
+    WOLFRAMITE(Strings.ORE_WOLFRAMITE_NAME, "oreTungsten"),
+    SCHEELITE(Strings.ORE_SCHEELITE_NAME, "oreTungsten"),
+
+    ILMENITE(Strings.ORE_ILMENITE_NAME, "oreTitanium"),
+    RUTILE(Strings.ORE_RUTILE_NAME, "oreTitanium"),
+
+    CALAVERITE(Strings.ORE_CALAVERITE_NAME, "oreGold"),
+    SYLVANITE(Strings.ORE_SYLVANITE_NAME, "oreGold"),
+    PUREGOLD(Strings.ORE_PUREGOLD_NAME, "oreGold"),
+
+    MAGNETITE(Strings.ORE_MAGNETITE_NAME, "oreIron"),
+    HEMATITE(Strings.ORE_HEMATITE_NAME, "oreIron"),
+    SIDERITE(Strings.ORE_SIDERITE_NAME, "oreIron"),
+    CHROMITE(Strings.ORE_CHROMITE_NAME, "oreIron"),
+    PENTLADITE(Strings.ORE_PENTLADITE_NAME, "oreIron"),
+
+    PYROLUSITE(Strings.ORE_PYROLUSITE_NAME, "stone"),
+    CINNABAR(Strings.ORE_CINNABAR_NAME, "stone"),
+    DOLOMITE(Strings.ORE_DOLOMITE_NAME, "stone"),
+    SPHALERITE(Strings.ORE_SPHALERITE_NAME, "stone"),
+    LIMESTONE(Strings.ORE_LIMESTONE_NAME, "stone"),
+    LIME(Strings.ORE_LIME_NAME, "stone"),
+    COBALTITE(Strings.ORE_COBALTITE_NAME, "stone"),
+
+    LITHIUM(Strings.ORE_LITHIUM_NAME, "clay"),
+
+    LAZURITE(Strings.ORE_LAZURITE_NAME, "oreLapis"),
+
+    BERYL(Strings.ORE_BERYL_NAME, "oreEmerald"),
+
     LIGNITE(Strings.ORE_LIGNITE_NAME, "oreCoal"),
     SUBBITUMINOUS(Strings.ORE_SUBBITUMINOUS_NAME, "oreCoal"),
     BITUMINOUS(Strings.ORE_BITUMINOUS_NAME, "oreCoal"),
