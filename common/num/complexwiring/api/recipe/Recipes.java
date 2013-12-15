f4d162414f937be84dfc3989fdbee5c7fc59198d
Numerios <MrNumerios@gmail.com>
2013-12-15 20:10:27 +0100

    External recipe adding tweaked

--- common/num/complexwiring/api/recipe/Recipes.java
+++ common/num/complexwiring/api/recipe/Recipes.java
@@ -2,7 +2,6 @@ package num.complexwiring.api.recipe;
 
 import cpw.mods.fml.common.Loader;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Recipes {
@@ -15,14 +14,8 @@ public class Recipes {
                 Method add = recipeManager.getMethod("add", ICWRecipe.class);
                 add.invoke(null, recipe);
                 return true;
-            } catch (NoSuchMethodException e) {
-                System.err.println("[CW-API] Couldn't add a recipe remotely!");
-            } catch (ClassNotFoundException e) {
-                System.err.println("[CW-API] Couldn't add a recipe remotely!");
-            } catch (InvocationTargetException e) {
-                System.err.println("[CW-API] Couldn't add a recipe remotely!");
-            } catch (IllegalAccessException e) {
-                System.err.println("[CW-API] Couldn't add a recipe remotely!");
+            } catch (Exception e) {
+                System.err.println("[CW] [API] Couldn't add a recipe remotely! " + e.printStackTrace());
             }
         }
         return false;
