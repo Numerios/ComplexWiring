a41e9892f3ff0685b19e14b8468a0112e0194c58
Numerios <MrNumerios@gmail.com>
2013-12-22 15:03:05 +0100

    A new slot type!

---
+++ common/num/complexwiring/client/slot/SlotSpecific.java
@@ -0,0 +1,33 @@
+package num.complexwiring.client.slot;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+public class SlotSpecific extends Slot {
+    private final ItemStack specific;
+    private final boolean canTake;
+
+    public SlotSpecific(IInventory inventory, ItemStack is, boolean canTake, int ID, int x, int y) {
+        super(inventory, ID, x, y);
+        this.specific = is;
+        this.canTake = canTake;
+    }
+
+    @Override
+    public boolean isItemValid(ItemStack is) {
+        return specific.itemID == is.itemID;
+    }
+
+    @Override
+    public int getSlotStackLimit() {
+        return Math.max(specific.getMaxStackSize(), 64); //sanity check
+    }
+
+    @Override
+    public boolean canTakeStack(EntityPlayer player) {
+        return canTake;
+    }
+
+}
