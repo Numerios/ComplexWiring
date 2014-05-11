927d3a74abe34f47c6fd8e9c2a03b88babb08709
Numerios <MrNumerios@gmail.com>
2014-05-11 17:36:48 +0200

    RGBACol can now have just RGB int output and version in mcmod.info is now a token

--- common/num/complexwiring/lib/Reference.java
+++ common/num/complexwiring/lib/Reference.java
@@ -3,7 +3,7 @@ package num.complexwiring.lib;
 public class Reference {
     public static final String MOD_ID = "ComplexWiring";
     public static final String MOD_NAME = "Complex Wiring";
-    public static final String MOD_VERSION = "0.0.0";
+    public static final String MOD_VERSION = "@VERSION@";
     public static final String TEXTURE_PATH = MOD_ID.toLowerCase() + ":";
 
     public static final String CLIENT_PROXY_CLASS = "num.complexwiring.core.proxy.ClientProxy";
