251c67674709364b9ce899921de719f52ce355db
Numerios <MrNumerios@gmail.com>
2014-05-17 17:04:42 +0200

    Better logger (apache log4j)

--- common/num/complexwiring/core/Logger.java
+++ common/num/complexwiring/core/Logger.java
@@ -2,11 +2,11 @@ package num.complexwiring.core;
 
 import num.complexwiring.ComplexWiring;
 import num.complexwiring.lib.Reference;
-
-import java.util.logging.Level;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 
 public class Logger {
-    private static java.util.logging.Logger cwLogger = java.util.logging.Logger.getLogger(Reference.MOD_ID);
+    private static final org.apache.logging.log4j.Logger cwLogger = LogManager.getLogger(Reference.MOD_ID);
 
     public static void init() {
     }
@@ -15,20 +15,20 @@ public class Logger {
         cwLogger.log(level, message);
     }
 
+    public static void error(String message) {
+        log(Level.ERROR, message);
+    }
+
+    public static void warn(String message) {
+        log(Level.WARN, message);
+    }
+
     public static void info(String message) {
         log(Level.INFO, message);
     }
 
-    public static void warn(String message) {
-        log(Level.WARNING, message);
-    }
-
-    public static void fine(String message) {
-        log(Level.FINE, message);
-    }
-
     public static void debug(String message) {
         if (ComplexWiring.DEBUG)
-            log(Level.WARNING, "[DEBUG] " + message);
+            log(Level.DEBUG, message);
     }
 }
