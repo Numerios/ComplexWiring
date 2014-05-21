dd295eb19cce9a26d964a1f249a7fcf36bd28257
Numerios <MrNumerios@gmail.com>
2014-05-21 16:57:19 +0200

    Forgot to add the ClientProxy

--- common/num/complexwiring/core/proxy/ClientProxy.java
+++ common/num/complexwiring/core/proxy/ClientProxy.java
@@ -1,7 +1,16 @@
 package num.complexwiring.core.proxy;
 
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import num.complexwiring.client.RenderingHandler;
+import num.complexwiring.machine.storagebox.RenderStorageBox;
+import num.complexwiring.machine.storagebox.TileStorageBox;
+
 public class ClientProxy extends CommonProxy {
     @Override
-    public void initRendering() {
+    public void init() {
+        super.init();
+        RenderingRegistry.registerBlockHandler(new RenderingHandler());
+        ClientRegistry.bindTileEntitySpecialRenderer(TileStorageBox.class, new RenderStorageBox());
     }
 }
