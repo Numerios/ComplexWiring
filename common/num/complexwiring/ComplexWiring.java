725386c770d34ab92887fc8e947ee2c4f15a7bf3
Numerios <MrNumerios@gmail.com>
2014-05-25 22:11:14 +0200

    Packets! (thank god for the forge wiki)

--- common/num/complexwiring/ComplexWiring.java
+++ common/num/complexwiring/ComplexWiring.java
@@ -12,6 +12,7 @@ import net.minecraftforge.common.MinecraftForge;
 import num.complexwiring.core.GuiHandler;
 import num.complexwiring.core.Logger;
 import num.complexwiring.core.ModuleManager;
+import num.complexwiring.core.network.PacketPipeline;
 import num.complexwiring.core.proxy.CommonProxy;
 import num.complexwiring.lib.Reference;
 import num.complexwiring.power.electrical.NetworkTickHandler;
@@ -19,6 +20,7 @@ import num.complexwiring.power.electrical.NetworkTickHandler;
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 public class ComplexWiring {
     public static final boolean DEBUG = true;
+    public static final PacketPipeline packetPipeline = new PacketPipeline();
     @Instance(Reference.MOD_ID)
     public static ComplexWiring instance;
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
@@ -27,27 +29,33 @@ public class ComplexWiring {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         Logger.init();
-
-        Logger.debug("PreInit!");
+        Logger.debug("PreInit started!");
 
         ModuleManager.preInit();
         //GameRegistry.registerPlayerTracker(new PlayerTracker());
+
+        Logger.debug("PreInit finished!");
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        Logger.debug("Init!");
+        Logger.debug("Init started!");
+        proxy.init();
+        packetPipeline.init();
 
         NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandler());
         MinecraftForge.EVENT_BUS.register(NetworkTickHandler.INSTANCE);
 
         ModuleManager.init();
-        proxy.init();
+        Logger.debug("Init finished!");
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        Logger.debug("PostInit!");
+        Logger.debug("PostInit started!");
+        packetPipeline.postInit();
+
         ModuleManager.postInit();
+        Logger.debug("PostInit finished!");
     }
 }
