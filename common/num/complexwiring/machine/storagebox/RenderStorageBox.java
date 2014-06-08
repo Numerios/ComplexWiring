379685fb428d71a77ff71e21576a0cb8f6e8095e
JBuk <jbuk@jbuk.eu>
2014-06-08 15:59:55 +0200

    Overlay on StorageBox won't be rendered, when block in front

--- common/num/complexwiring/machine/storagebox/RenderStorageBox.java
+++ common/num/complexwiring/machine/storagebox/RenderStorageBox.java
@@ -2,10 +2,11 @@ package num.complexwiring.machine.storagebox;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import num.complexwiring.api.vec.Vector3;
 import num.complexwiring.client.RenderOverlay;
-import num.complexwiring.core.Logger;
 import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
@@ -16,7 +17,12 @@ public class RenderStorageBox extends TileEntitySpecialRenderer {
         if (tile instanceof TileStorageBox) {
             GL11.glPushMatrix();
             TileStorageBox tileStorageBox = (TileStorageBox) tile;
-            RenderOverlay.renderISWithTextOnSide(tileStorageBox, tileStorageBox.getContaining(), x, y, z, tileStorageBox.getFacing());
+
+            Vector3 facingTowardsTo = tileStorageBox.pos().add(tileStorageBox.getFacing().offsetX, tileStorageBox.getFacing().offsetY, tileStorageBox.getFacing().offsetZ);
+            Block block = tileStorageBox.world().getBlock(facingTowardsTo.getX(), facingTowardsTo.getY(), facingTowardsTo.getZ());
+            if (!block.isOpaqueCube()) {
+                RenderOverlay.renderISWithTextOnSide(tileStorageBox, tileStorageBox.getContaining(), x, y, z, tileStorageBox.getFacing());
+            }
             GL11.glPopMatrix();
         }
     }
