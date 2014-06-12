84321191857e5516b3a1be7370aa5cffade151f1
Numerios <MrNumerios@gmail.com>
2014-06-12 17:29:03 +0200

    Smasher now can destroy items in storage boxes!

--- common/num/complexwiring/machine/smasher/TileSmasher.java
+++ common/num/complexwiring/machine/smasher/TileSmasher.java
@@ -16,6 +16,7 @@ import num.complexwiring.api.recipe.SmasherRecipe;
 import num.complexwiring.api.vec.Vector3;
 import num.complexwiring.base.ItemScrewdriver;
 import num.complexwiring.core.Logger;
+import num.complexwiring.machine.storagebox.TileStorageBox;
 import num.complexwiring.recipe.RecipeManager;
 
 import java.util.ArrayList;
@@ -38,19 +39,21 @@ public class TileSmasher extends TileEntityBase implements IFacing {
             Vector3 facingVec = pos().clone().step(facing);
             if (recipe == null) {
                 if (RecipeManager.get(RecipeManager.Type.SMASHER, facingVec.getIS(world())) != null) {
-                    recipe = (SmasherRecipe) RecipeManager.get(RecipeManager.Type.SMASHER, facingVec.getIS(world()));
-                    if (recipe != null) { // + hasPower
-                        recipeNeededPower = recipe.getNeededPower();
-                        recipeOutput = recipe.getOutput(new Random());
-                        processTime = 0;
-                    } else {
-                        recipe = null;
+                    trySetupRecipe(facingVec.getIS(world()));
+                } else if (facingVec.toTile(world()) instanceof TileStorageBox) {
+                    TileStorageBox storageBox = (TileStorageBox) facingVec.toTile(world());
+                    ItemStack is = storageBox.getContaining();
+                    if (is != null) {
+                        is.stackSize = 1;
+                        trySetupRecipe(is);
                     }
                 }
             }
-            if (recipe != null && facingVec.getIS(world()).isItemEqual(recipe.getInput())) {
+            if (recipe != null && (facingVec.getIS(world()).isItemEqual(recipe.getInput()) || facingVec.toTile(world()) instanceof TileStorageBox)) {
                 processTime++;
-                world().destroyBlockInWorldPartially(0, facingVec.getX(), facingVec.getY(), facingVec.getZ(), (int) getProgress(10));
+                if (!(facingVec.toTile(world()) instanceof TileStorageBox)) {
+                    world().destroyBlockInWorldPartially(0, facingVec.getX(), facingVec.getY(), facingVec.getZ(), (int) getProgress(10));
+                }
                 if (processTime >= recipeNeededPower) {
                     endProcessing();
                 }
@@ -63,6 +66,19 @@ public class TileSmasher extends TileEntityBase implements IFacing {
 
     }
 
+    private void trySetupRecipe(ItemStack is) {
+        if (RecipeManager.get(RecipeManager.Type.SMASHER, is) != null) {
+            recipe = (SmasherRecipe) RecipeManager.get(RecipeManager.Type.SMASHER, is);
+            if (recipe != null) { // + hasPower
+                recipeNeededPower = recipe.getNeededPower();
+                recipeOutput = recipe.getOutput(new Random());
+                processTime = 0;
+            } else {
+                recipe = null;
+            }
+        }
+    }
+
     private void resetProcessing() {
         recipe = null;
         recipeOutput.clear();
@@ -72,18 +88,25 @@ public class TileSmasher extends TileEntityBase implements IFacing {
 
 
     public void endProcessing() {
+        Vector3 facingVec = pos().clone().step(facing);
         if (recipeOutput != null && recipeOutput.size() > 0) {
             for (ItemStack output : recipeOutput) {
                 if (output != null && output.stackSize != 0) {
-                    Vector3 blockPos = pos().clone().step(facing);
-                    world().destroyBlockInWorldPartially(0, blockPos.getX(), blockPos.getY(), blockPos.getZ(), 10);
-                    EntityItem entityItem = new EntityItem(world(), blockPos.getX() + 0.5D, blockPos.getY() + 0.5D, blockPos.getZ() + 0.5D, output);
+                    if (facingVec.toTile(world()) instanceof TileStorageBox) {
+                        world().destroyBlockInWorldPartially(0, facingVec.getX(), facingVec.getY(), facingVec.getZ(), 10);
+                    }
+                    EntityItem entityItem = new EntityItem(world(), facingVec.getX() + 0.5D, facingVec.getY() + 0.5D, facingVec.getZ() + 0.5D, output);
                     entityItem.setVelocity(0D, 0D, 0D);
                     entityItem.delayBeforeCanPickup = 0;
                     world().spawnEntityInWorld(entityItem);
                 }
             }
-            pos().clone().step(facing).setBlock(world(), Blocks.air);
+            if (facingVec.toTile(world()) instanceof TileStorageBox) {
+                TileStorageBox storageBox = (TileStorageBox) facingVec.toTile(world());
+                storageBox.decrStackSize(0, 1);
+            } else {
+                facingVec.setBlock(world(), Blocks.air);
+            }
         }
         resetProcessing();
     }
