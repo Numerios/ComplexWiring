b8cf761872473388049440d4826d1f921168d013
Numerios <MrNumerios@gmail.com>
2014-06-13 16:09:43 +0200

    Removed research

--- common/num/complexwiring/tablet/ModuleTablet.java
+++ common/num/complexwiring/tablet/ModuleTablet.java
@@ -1,16 +1,10 @@
 package num.complexwiring.tablet;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import num.complexwiring.lib.Module;
 import num.complexwiring.lib.Strings;
-import num.complexwiring.tablet.guidebook.GuidebookPageRegistry;
 import num.complexwiring.tablet.guidebook.ItemGuidebook;
-import num.complexwiring.tablet.research.Research;
-import num.complexwiring.tablet.research.ResearchManager;
-import num.complexwiring.tablet.research.SolidResearchTask;
 import num.complexwiring.tablet.startpaper.ItemStartPaper;
 import num.complexwiring.tablet.tablet.ItemTablet;
 
@@ -26,20 +20,6 @@ public class ModuleTablet extends Module {
     @Override
     public void init() {
         registerRecipes();
-
-        ResearchManager.init();
-        ResearchManager.addAll(
-                new Research(Research.Tier.TIER_0, "First",
-                        new SolidResearchTask("Stone", new ItemStack(Blocks.stone)),
-                        new SolidResearchTask("Wood", new ItemStack(Blocks.log))
-                ),
-                new Research(Research.Tier.TIER_0, "Second",
-                        new SolidResearchTask("Stone", new ItemStack(Blocks.stone)),
-                        new SolidResearchTask("Wood", new ItemStack(Blocks.log))
-                )
-        );
-
-        GuidebookPageRegistry.init();
     }
 
     private void registerBlocks() {
