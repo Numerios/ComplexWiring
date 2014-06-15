b888314ed6bb010e52aad169437c49652342eaf2
Numerios <MrNumerios@gmail.com>
2014-06-15 16:30:58 +0200

    One more time
    *hey, we're gonna celebra-a-a-ate*

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -2,10 +2,11 @@
 # This script is for CW's jenkins CI system that took a lot of time to set up
 # If you are building this mod yourself, just ignore this...
 echo "Preparing to build"
+chmod +x gradlew
 if [ ${GIT_BRANCH} == "origin/dev" ]; then
    echo "Building from dev (_alpha.xx)!"
-   sh gradlew build -PisDev=1 -S
+   sh gradlew clean setupDecompWorkspace build -PisDev=1 -S
 elif [ ${GIT_BRANCH} == "origin/master" ];  then
    echo "Building from master (.xx)!"
-   sh gradlew build -S
+   sh gradlew clean setupDecompWorkspace build -S
 fi
