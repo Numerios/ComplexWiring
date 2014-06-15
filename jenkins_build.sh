53743985d94084bc37e5791344845677350a3b88
JBukCZ <jbuk@jbuk.eu>
2014-06-15 16:45:54 +0200

    It's my turn to fix it :\

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -2,7 +2,7 @@
 # This script is for CW's jenkins CI system that took a lot of time to set up
 # If you are building this mod yourself, just ignore this...
 echo "Preparing to build"
-chmod +x gradlew
+chmod +x ./gradlew
 if [ ${GIT_BRANCH} == "origin/dev" ]; then
    echo "Building from dev (_alpha.xx)!"
    sh gradlew clean setupDecompWorkspace build -PisDev=1 -S
