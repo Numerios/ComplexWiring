b80b4f3d0e8253a72fe826c254feae9f53aeb545
Numerios <MrNumerios@gmail.com>
2014-06-15 16:12:09 +0200

    Hopefully fixed the jenkins build script

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # This script is for CW's jenkins CI system that took a lot of time to set up
 # If you are building this mod yourself, just ignore this...
-if [ ${GIT_BRANCH} == "dev" ]; then
+echo "Preparing to build"
+if [ ${GIT_BRANCH} == "origin/dev" ]; then
    echo "Building from dev (_alpha.xx)!"
    sh ./gradlew.sh build -PisDev=1 -S
-elif [ ${GIT_BRANCH} == "master" ];  then
+elif [ ${GIT_BRANCH} == "origin/master" ];  then
    echo "Building from master (.xx)!"
    sh ./gradlew.sh build -S
 fi
