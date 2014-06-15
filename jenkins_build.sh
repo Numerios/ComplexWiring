e977ab8b8e6ae708dbeac2242245608d156de44f
Numerios <MrNumerios@gmail.com>
2014-06-15 16:07:43 +0200

    Getting there

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# This script is for CW's jenkins CI system that took a lot of time to set up
+# If you are building this mod yourself, just ignore this...
 if [ ${GIT_BRANCH} == "dev" ]; then
    echo "Building from dev (_alpha.xx)!"
-   sh ./gradlew.sh build -PisDev=1
+   sh ./gradlew.sh build -PisDev=1 -S
 elif [ ${GIT_BRANCH} == "master" ];  then
    echo "Building from master (.xx)!"
-   sh ./gradlew.sh build
+   sh ./gradlew.sh build -S
 fi
