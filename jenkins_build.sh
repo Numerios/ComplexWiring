9b62111df43c11a1abd88fc9128dea99ebe88d54
Numerios <MrNumerios@gmail.com>
2014-06-15 16:02:10 +0200

    Just a little more

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ${GIT_BRANCH} == "dev" ]; then
    echo "Building from dev (_alpha.xx)!"
-   sh ../gradlew.sh build -PisDev=1
+   sh ./gradlew.sh build -PisDev=1
 elif [ ${GIT_BRANCH} == "master" ];  then
    echo "Building from master (.xx)!"
-   sh ../gradlew.sh build
+   sh ./gradlew.sh build
 fi
