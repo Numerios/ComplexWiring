1acb453b4807bc8ed33951f83d91e6a712523810
JBukCZ <jbuk@jbuk.eu>
2014-06-30 19:05:19 +0200

    CW name is officially without space

--- common/num/complexwiring/lib/Reference.java
+++ common/num/complexwiring/lib/Reference.java
@@ -2,7 +2,7 @@ package num.complexwiring.lib;
 
 public class Reference {
     public static final String MOD_ID = "ComplexWiring";
-    public static final String MOD_NAME = "Complex Wiring";
+    public static final String MOD_NAME = "ComplexWiring";
     public static final String MOD_VERSION = "@VERSION@";
     public static final String TEXTURE_PATH = MOD_ID.toLowerCase() + ":";
 
