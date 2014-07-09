dbe22a2ba557a7554151ebe57905dd05f7b70064
Numerios <MrNumerios@gmail.com>
2014-07-09 20:54:29 +0200

    Removed unused network code
    It needs a rewrite because this code is reeeaaaally old and buggy (and it is said that a new way to do this has been found)

--- common/num/complexwiring/ComplexWiring.java
+++ common/num/complexwiring/ComplexWiring.java
@@ -16,7 +16,6 @@ import num.complexwiring.core.ConfigHandler;
 import num.complexwiring.core.GuiHandler;
 import num.complexwiring.core.Logger;
 import num.complexwiring.core.ModuleManager;
-import num.complexwiring.core.network.PacketPipeline;
 import num.complexwiring.core.proxy.CommonProxy;
 import num.complexwiring.lib.Reference;
 import num.complexwiring.mechanical.ModuleMechanical;
@@ -27,7 +26,6 @@ import java.io.File;
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, guiFactory = Reference.GUI_CONFIG_CLASS)
 public class ComplexWiring {
     public static final boolean DEBUG = true;
-    public static final PacketPipeline packetPipeline = new PacketPipeline();
     @Instance(Reference.MOD_ID)
     public static ComplexWiring instance;
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
@@ -57,7 +55,6 @@ public class ComplexWiring {
     public void init(FMLInitializationEvent event) {
         Logger.debug("Init started!");
         proxy.init();
-        packetPipeline.init();
 
         NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiHandler());
 
@@ -69,7 +66,6 @@ public class ComplexWiring {
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         Logger.debug("PostInit started!");
-        packetPipeline.postInit();
 
         ModuleManager.INSTANCE.postInit();
         Logger.debug("I'm ready to rock!");
