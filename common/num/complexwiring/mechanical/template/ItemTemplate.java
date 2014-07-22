87ad90a4c361666dfec2eeec1a4d70fc6284f631
Numerios <MrNumerios@gmail.com>
2014-07-22 20:49:52 +0200

    Just a bit better templates

--- common/num/complexwiring/mechanical/template/ItemTemplate.java
+++ common/num/complexwiring/mechanical/template/ItemTemplate.java
@@ -8,7 +8,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.IIcon;
-import net.minecraft.world.World;
 import num.complexwiring.base.ModuleBase;
 import num.complexwiring.lib.Reference;
 import num.complexwiring.mechanical.ModuleMechanical;
@@ -27,13 +26,6 @@ public class ItemTemplate extends Item {
     }
 
     @Override
-    public void onCreated(ItemStack is, World world, EntityPlayer player) {
-        is.stackTagCompound = new NBTTagCompound();
-        is.stackTagCompound.setBoolean("isEmpty", true);
-        super.onCreated(is, world, player);
-    }
-
-    @Override
     public void registerIcons(IIconRegister ir) {
         icon_empty = ir.registerIcon(Reference.TEXTURE_PATH + "template_empty");
         icon_full = ir.registerIcon(Reference.TEXTURE_PATH + "template_full");
@@ -49,13 +41,13 @@ public class ItemTemplate extends Item {
         return icon_empty;
     }
 
-   /* @Override
+    @Override
     public IIcon getIcon(ItemStack is, int pass) {
-        if (!isEmpty(is)) {
+        if (hasContents(is)) {
             return icon_full;
         }
         return icon_empty;
-    }                   */
+    }
 
     @Override
     public String getUnlocalizedName(ItemStack is) {
@@ -64,61 +56,58 @@ public class ItemTemplate extends Item {
 
     @Override
     public void getSubItems(Item item, CreativeTabs tab, List list) {
-        list.add(createEmpty());
-        list.add(create(new ItemStack(Items.diamond)));
-    }
-
-    private static boolean hasNBT(ItemStack is) {
-        return is.stackTagCompound != null;
-    }
-
-    public static boolean isEmpty(ItemStack template) {
-        return hasNBT(template) && template.stackTagCompound.getBoolean("isEmpty");
-    }
-
-    public static boolean setContents(ItemStack template, ItemStack contents) {
-        if (!isEmpty(template)) {
-            return false;
-        }
-        NBTTagCompound contentsTag = new NBTTagCompound();
-        contents.writeToNBT(contentsTag);
-        template.stackTagCompound.setTag("contents", contentsTag);
-        template.stackTagCompound.setBoolean("isEmpty", false);
-        return true;
-    }
-
-    public static ItemStack getContents(ItemStack template) {
-        if (isEmpty(template)) {
-            return null;
-        }
-        NBTTagCompound contentsTag = (NBTTagCompound) template.stackTagCompound.getTag("contents");
-        return ItemStack.loadItemStackFromNBT(contentsTag);
-    }
-
-    public static ItemStack createEmpty() {
-        ItemStack template = new ItemStack(ModuleMechanical.template);
-        template.stackTagCompound = new NBTTagCompound();
-        template.stackTagCompound.setBoolean("isEmpty", true);
-        return template;
-    }
-
-    public static ItemStack create(ItemStack contents) {
-        ItemStack template = createEmpty();
-        setContents(template, contents);
-        return template;
+        ItemStack empty = create();
+        list.add(empty);
+        ItemStack diamond = create();
+        setContents(diamond, new ItemStack(Items.diamond));
+        list.add(diamond);
     }
 
     @Override
     public void addInformation(ItemStack is, EntityPlayer player, List list, boolean b) {
-        if (isEmpty(is)) {
-            list.add("EMPTY");
-        } else {
+        if (hasContents(is)) {
             list.add("Contents:");
-            ItemStack contents = getContents(is);
-            if (contents != null) {
-                list.add("" + getContents(is).toString());
-            }
+            list.add("" + getContents(is, false));
+        } else {
+            list.add("empty");
         }
         super.addInformation(is, player, list, b);
+    }
+
+    public static ItemStack create() {
+        ItemStack is = new ItemStack(ModuleMechanical.template);
+        is.stackTagCompound = new NBTTagCompound();
+        is.stackTagCompound.setBoolean("hasContents", false);
+        return is;
+    }
+
+    public static boolean hasContents(ItemStack template) {
+        if (template.stackTagCompound == null || !template.stackTagCompound.getBoolean("hasContents")) {
+            return false;
+        }
+        return template.stackTagCompound.getBoolean("hasContents");
+    }
+
+    public static boolean setContents(ItemStack template, ItemStack contents) {
+        if (template.stackTagCompound == null || template.stackTagCompound.getBoolean("hasContents") || contents == null) {
+            return false;
+        }
+        NBTTagCompound contentsNBT = new NBTTagCompound();
+        contents.writeToNBT(contentsNBT);
+        template.stackTagCompound.setTag("contents", contentsNBT);
+        template.stackTagCompound.setBoolean("hasContents", true);
+        return true;
+    }
+
+    public static ItemStack getContents(ItemStack template, boolean remove) {
+        if (template.stackTagCompound == null || !template.stackTagCompound.getBoolean("hasContents")) {
+            return null;
+        }
+        ItemStack contents = ItemStack.loadItemStackFromNBT(template.stackTagCompound.getCompoundTag("contents"));
+        if (remove) {
+            template.stackTagCompound.setBoolean("hasContents", false);
+            template.stackTagCompound.setTag("contents", null);
+        }
+        return contents;
     }
 }
